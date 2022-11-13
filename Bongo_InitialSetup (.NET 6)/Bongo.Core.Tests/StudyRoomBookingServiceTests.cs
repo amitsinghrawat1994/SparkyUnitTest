89318bf556994030614bf9484bbcbc41912b9bc1
Amit Singh Rawat <amitsinghrawat99@gmail.com>
2022-11-13 21:39:53 +0530

    81. 15 Bongo Core Save Booking Unit Test

--- Bongo_InitialSetup (.NET 6)/Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
+++ Bongo_InitialSetup (.NET 6)/Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -2,6 +2,7 @@
 
 using Bongo.Core.Services;
 using Bongo.DataAccess.Repository.IRepository;
+using Bongo.Models.Model;
 using Moq;
 using NUnit.Framework;
 
@@ -10,6 +11,8 @@ namespace Bongo.Core.Tests
     [TestFixture]
     public class StudyRoomBookingServiceTests
     {
+        private StudyRoomBooking _request;
+        private List<StudyRoom> _availalbeStudyRoom;
         private Mock<IStudyRoomBookingRepository> _studyRoomBookingRepoMock;
         private Mock<IStudyRoomRepository> _studyRoomRepoMock;
 
@@ -18,8 +21,29 @@ namespace Bongo.Core.Tests
         [SetUp]
         public void Setup()
         {
+            _request = new StudyRoomBooking
+            {
+                FirstName = "Ben",
+                LastName = "Spark",
+                Email = "ben@gmail.com",
+                Date = new DateTime(2022, 1, 1)
+            };
+
+            _availalbeStudyRoom = new List<StudyRoom> {
+                new StudyRoom
+                {
+                    Id = 10,
+                    RoomName = "Michigan",
+                    RoomNumber = "A202"
+                } 
+            };
+
             _studyRoomBookingRepoMock = new Mock<IStudyRoomBookingRepository>();
+            
             _studyRoomRepoMock = new Mock<IStudyRoomRepository>();
+            _studyRoomRepoMock.Setup(x => x.GetAll())
+                .Returns(_availalbeStudyRoom);
+
 
             _bookingService = new StudyRoomBookingService(
                 _studyRoomBookingRepoMock.Object,
@@ -31,7 +55,7 @@ namespace Bongo.Core.Tests
         public void GetAllBooking_InvokeMethod_CheckIfRepoIsCalled()
         {
             _bookingService.GetAllBooking();
-            _studyRoomBookingRepoMock.Verify(x => x.GetAll(null), Times.Once); 
+            _studyRoomBookingRepoMock.Verify(x => x.GetAll(null), Times.Once);
         }
 
         [Test]
@@ -42,6 +66,31 @@ namespace Bongo.Core.Tests
 
             Assert.AreEqual("Value cannot be null. (Parameter 'request')", exception.Message);
             Assert.AreEqual("request", exception.ParamName);
+        }
+
+        [Test]
+        public void StudyRoomBooking_SaveBookingWithAvailableRoom_ReturnResultWithAllValue()
+        {
+            //asert
+            StudyRoomBooking saveStudyRoomBooking = null;
+
+            _studyRoomBookingRepoMock.Setup(x => x.Book(It.IsAny<StudyRoomBooking>()))
+                .Callback<StudyRoomBooking>(booking =>
+                {
+                    saveStudyRoomBooking = booking;
+                });
+
+
+            // act
+            _bookingService.BookStudyRoom(_request);
+
+            //assert
+            _studyRoomBookingRepoMock.Verify(x => x.Book(It.IsAny<StudyRoomBooking>()), Times.Once);
+
+            Assert.NotNull(saveStudyRoomBooking);
+            Assert.AreEqual(_request.FirstName, saveStudyRoomBooking.FirstName);
+
+            Assert.AreEqual(_availalbeStudyRoom.First().Id, saveStudyRoomBooking.StudyRoomId);
         }
 
     }
